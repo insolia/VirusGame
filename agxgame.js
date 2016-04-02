bc2263efb1ec2285272eb64b853363ed430d85cb
Yegor <eaniconer@yandex.ru>
2016-04-02 06:35:58 +0300

    without gameover

---
+++ agxgame.js
@@ -0,0 +1,47 @@
+var io;
+var gameSocket;
+
+exports.initGame = function(sio, socket){
+	io = sio;
+	gameSocket = socket;
+	gameSocket.emit('connected', {message: "You are connected"});
+
+
+	// События host'a
+	gameSocket.on('hostCreateNewGame', hostCreateNewGame);
+	gameSocket.on('Answered', giveAnswer);
+
+	// События client'a
+	gameSocket.on('playerJoinGame', playerJoinGame);
+	//gameSocket.on('playerAnswered', playerAnswer);
+}
+
+
+function hostCreateNewGame() {
+	var thisGameId = (Math.random() * 10000) | 0;
+
+	this.emit('newGameCreated', {gameId: thisGameId, mySocketId: this.id});
+
+	this.join(thisGameId.toString());
+}
+
+
+function playerJoinGame(data){
+	var sock = this;
+	var room = gameSocket.manager.rooms["/" + data.gameId];
+
+	if (room != undefined) {
+		data.mySoketId = sock.id;
+		sock.join(data.gameId);
+
+		io.sockets.in(data.gameId).emit('beginGame', data);
+	} else {
+		//error
+	}
+
+}
+
+
+function giveAnswer(data){
+		io.sockets.in(data.gameId).emit('gamming', data);
+}
